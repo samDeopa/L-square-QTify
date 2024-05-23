40e7250a284306f51e6b61c65527039b009dba7e
samDeopa <sameerchandeopa@gmail.com>
2024-05-23 14:09:29 +0530

    added new Album section and collapse button

--- qtify/src/pages/Home/Home.js
+++ qtify/src/pages/Home/Home.js
@@ -4,13 +4,20 @@ import Hero from "../../components/Hero/Hero";
 import Section from "../../components/Section/Section";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Button } from "@mui/material";
 export default function Home() {
   const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbum, setNewAlbum] = useState([]);
   useEffect(() => {
     axios
       .get("https://qtify-backend-labs.crio.do/albums/top")
       .then((response) => {
         setTopAlbums(response.data);
+      });
+    axios
+      .get("https://qtify-backend-labs.crio.do/albums/new")
+      .then((response) => {
+        setNewAlbum(response.data);
       });
   }, []);
 
@@ -19,8 +26,18 @@ export default function Home() {
       <Navbar></Navbar>
       <Hero></Hero>
       <div className={styles.section}>
-        <div className={styles.title}>Top Albums</div>
+        <div className={styles.title}>
+          <p>Top Albums</p>
+          <Button>Collapse</Button>
+        </div>
         <Section data={topAlbums} type="album" />
+      </div>
+      <div className={styles.section}>
+        <div className={styles.title}>
+          <p>New Albums</p>
+          <Button>Collapse</Button>
+        </div>
+        <Section data={newAlbum} type="album" />
       </div>
     </div>
   );
